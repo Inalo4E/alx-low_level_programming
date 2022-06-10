57db1fb12c3bb87b305a6b462b34afc93ba5d105
Emmanuel Inalo <emmainalo@gmail.com>
2022-06-10 10:10:50 -0700

    Numbers print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers between 0 and 9.
+ * @int - prints numbers.
+ *
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
