ab59e576e2ea62dfcaf77e319a3950c157264860
Emmanuel Inalo <emmainalo@gmail.com>
2022-06-22 05:16:00 -0700

    Written to print alphabets in lowercase using putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-
 /**
  * main - Prints the alphabet.
+ * @putchar - program to print alphabets in lowercase
  *
- * Return: Always 0 (Success)
+ * Return: Always 0(success)
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	int putchar(int ch);
+	int ch;
 
-	for (i = 0; i < 26; i++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		putchar(alp[i]);
+		putchar(ch);
 	}
-	putchar('\n');
 	return (0);
 }
