9948872652f531c01eff4c6ba1cb6306f6a91057
Emmanuel Inalo <emmainalo@gmail.com>
2022-06-24 15:58:48 +0100

    Function print its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print filename.
+ * @argc: number of arguments entered in command line.
+ * @argv: holds the string value of arguments entered.
+ *
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	argc = argc;
+
+	return (0);
+}
